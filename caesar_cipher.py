ba58ecbe25217b4f4da7c5fab97f9ce075e9bbed
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-10 20:34:16 +0700

    Add simple user interface

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,10 +2,6 @@ import caesar_art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type shift alphabet number:\n"))
-
 def caesar_cipher(text, shift, direction):
     # проверяем, выходит ли сдвиг за рамки списка.
     if shift > len(alphabet) - 1:
@@ -23,5 +19,13 @@ def caesar_cipher(text, shift, direction):
         message += alphabet[index]
     return message
 
+again = "yes"
 print(caesar_art.logo)
-print(caesar_cipher(text, shift, direction))
+
+while again == "yes" or again == "y":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type shift alphabet number:\n"))
+    print(caesar_cipher(text, shift, direction))
+    again = input("Again?(yes/no)\n").lower()
+print("Goodbye!\n")
